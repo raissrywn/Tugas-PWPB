cb4d44fb73cd2018197d34c374786a05e9ed2f2e
Ammar Rais <55770661+raissrywn@users.noreply.github.com>
2020-04-09 11:28:04 +0700

    My Profile 1.2

--- aboutme.php
+++ aboutme.php
@@ -3,5 +3,6 @@ echo '<br>';
 echo 'Nama : Ammar Rais Suryawan <br>';
 echo 'Kelas: XI RPL 1 <br>';
 echo 'Asal Sekolah : SMK Taruna Bhakti <br>';
-echo 'Alamat Rumah : Kampung Cilangkap No.01 RT 01 RW 13, Cilangkap, Tapos, Depok <br>';
+echo 'No HP : 0895327536974 <br>';
+echo 'Email : askmar.rais@gmail.com <br>';
 ?>
